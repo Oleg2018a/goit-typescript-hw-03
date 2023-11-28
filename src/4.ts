363396859a8312bc37700472739d2fd9e92ba62a
Oleg2018a <126179919+Oleg2018a@users.noreply.github.com>
2023-11-28 22:02:28 +0200

    .

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+
+
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Ви ввійшли до будинку.");
+    } else {
+      console.log("Двері закриті. Ввійти неможливо.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинено.");
+    } else {
+      console.log("Ключ не підходить. Двері залишаються закритими.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
